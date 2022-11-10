7528e3c71382373cb73e14be1a2ed5732473e087
Serveriii <n2jose00@students.oamk.fi>
2022-11-10 10:39:39 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+//Basic:
+//1.
+let content = document.getElementById("content");
+console.log(content.tagName);
+//2.
+let list = document.getElementsByClassName("basic");
+console.log(list[1].tagName);
+//3.
+let list3 = document.getElementsByTagName("li");
+console.log(list3[0].textContent);
+//4.
+let list2 = document.querySelector("ul");
+console.log(list2.lastElementChild.innerText);
+
+//Collection:
+//1.
+//2.
+//3.
+//4.
